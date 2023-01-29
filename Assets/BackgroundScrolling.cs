bb1a5995b19aeb242bbbd6cdbc6eabba90a952ec
Daniil Smolyakov <danonofficial@gmail.com>
2023-01-29 20:54:15 +0400

    added background scrolling

---
+++ Assets/BackgroundScrolling.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float scrollingSpeed = 0.5f;
+    Vector2 offset;
+    public AnimationCurve scrollingCurve;
+    void Start()
+    {
+        offset = GetComponent<SpriteRenderer>().material.mainTextureOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float boosterMaxTime = GameObject.FindGameObjectsWithTag("Player")[0].transform.GetComponent<ShootingScript>().speedBoosterMaxTime;
+        float boosterTimer = Mathf.Max(GameObject.FindGameObjectsWithTag("Player")[0].transform.GetComponent<ShootingScript>().speedBoosterTimer, 0);
+        boosterTimer = Mathf.Min(boosterMaxTime, boosterTimer);
+
+        float boosterCoefficient = boosterTimer / boosterMaxTime;
+        
+        float y = Mathf.Repeat(Time.time * (scrollingSpeed), 1);
+        // to do: scalable scrolling
+        Vector2 scrolledOffset = new Vector2(offset.x, y);
+        GetComponent<SpriteRenderer>().material.mainTextureOffset = scrolledOffset;
+    }
+}
